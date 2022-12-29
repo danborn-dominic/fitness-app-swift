e02a18536ac349dbbbb13a6f4b7c06075d0518c1
danborn-dominic <ddanborn@purdue.edu>
2022-12-28 20:04:45 -0700

    Initial Commit

---
+++ Fitness App/Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppApp.swift
+//  Fitness App
+//
+//  Created by Dominic Danborn on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
