d3dd8b7d812285433efb1ec97388f6c98b4c04fa
danborn-dominic <ddanborn@purdue.edu>
2022-12-28 20:18:20 -0700

    commit

--- Fitness App/Views/LaunchView.swift
+++ Fitness App/Views/LaunchView.swift
@@ -8,19 +8,18 @@
 import SwiftUI
 
 struct LaunchView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Text("Hello")
+        
     }
 }
 
 struct LaunchView_Previews: PreviewProvider {
+    
     static var previews: some View {
+        
         LaunchView()
     }
 }
